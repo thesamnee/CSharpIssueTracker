96570e3d15c1c9e4cd512a55a2aacf2774d3a394
Sam Nee <samuel.r.nee@gmail.com>
2020-06-17 20:35:36 -0400

    set up initial migration - Success!

---
+++ Migrations/20200618003309_InitialMigration.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace IssueTracker.Migrations
+{
+    public partial class InitialMigration : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Issues",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Title = table.Column<string>(maxLength: 70, nullable: false),
+                    Desc = table.Column<string>(nullable: false),
+                    Status = table.Column<string>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Issues", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Issues");
+        }
+    }
+}
--- Models/issueTracker.cs
+++ Models/issueTracker.cs
@@ -1,10 +1,18 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace IssueTracker.Models
 {
   public class Issue
   {
+      [Key]
       public int Id { get; set; }
+      [Required]
+      [MaxLength(70)]
       public string Title { get; set; }
+      [Required]
       public string Desc { get; set; }
+      [Required]
       public string Status { get; set; }
+
   }  
 }
