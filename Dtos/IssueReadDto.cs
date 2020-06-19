7d8bb671b374a2abff085d063c528a4ccdc6b4dd
Sam Nee <samuel.r.nee@gmail.com>
2020-06-19 18:31:31 -0400

    DTOs

---
+++ Dtos/IssueReadDto.cs
@@ -0,0 +1,11 @@
+namespace IssueTracker.Dtos
+{
+  public class IssueReadDto
+  {
+      public int Id { get; set; }
+      public string Title { get; set; }
+      public string Desc { get; set; }
+      public string Status { get; set; }
+
+  }  
+}
