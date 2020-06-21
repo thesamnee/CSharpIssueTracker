b9c8785ec65868c8016c6ed1fbf8593a8cf566b0
Sam Nee <samuel.r.nee@gmail.com>
2020-06-21 17:25:38 -0400

    create dto

---
+++ Dtos/IssueCreateDto.cs
@@ -0,0 +1,9 @@
+namespace IssueTracker.Dtos
+{
+  public class IssueCreateDto
+  {
+      public string Title { get; set; }
+      public string Desc { get; set; }
+      public string Status { get; set; }
+  }  
+}
